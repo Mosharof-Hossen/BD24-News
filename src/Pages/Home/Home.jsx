cfd26cb8c91fc880c2e8db06a15802e53a1f90b1
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-08-13 21:06:58 +0600

    Navbar added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,8 +9,10 @@ const Home = () => {
     return (
         <div >
             <Header></Header>
-            <Navbar></Navbar>
+            
             <BreakingNews></BreakingNews>
+
+            <Navbar></Navbar>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
                 <div>
                     <LeftSideNav></LeftSideNav>
