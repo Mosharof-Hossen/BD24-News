84ec31d74dc5c834185289beade6da3213e1e42a
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-08-13 20:37:21 +0600

    header done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 
 const Home = () => {
@@ -7,8 +9,18 @@ const Home = () => {
         <div >
             <Header></Header>
             <Navbar></Navbar>
-
-            This is Home
+ 
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+                <div>
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="md:col-span-2">
+                    News Coming Soon.......
+                </div>
+                <div>
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,9 +1,11 @@
-
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div className="font-pacifico">
-            Header
+        <div className=" text-center space-y-3 my-6">
+            <h1 className="text-6xl font-pacifico">The BD-24 News </h1>
+            <p className='text-[#706F6F]'>Journalism Without Fear or Favour</p>
+            <p>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
