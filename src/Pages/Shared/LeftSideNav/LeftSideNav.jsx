12789e9b026d4c8eb0f3960076baaf2cf8ad366d
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-08-14 13:56:18 +0600

    left side navbar news added

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import LeftSideNewsCard from "./LeftSideNewsCard";
 
 
 const LeftSideNav = () => {
     const [categories, seCategories] = useState([]);
+    const [news, setNews] = useState([]);
     useEffect(() => {
         fetch("categories.json")
             .then(res => res.json())
             .then(data => seCategories(data))
+        fetch("news.json")
+            .then(res => res.json())
+            .then(data => setNews(data.slice(10,13)))
     }, [])
-    console.log(categories);
+
     return (
         <div className="font-poppins p-4">
             <h3 className="text-xl font-bold mb-4">All Category</h3>
@@ -22,6 +27,12 @@ const LeftSideNav = () => {
                     categories?.map((category) => {
                         return <Link className="block text-[#9F9F9F]" to={`/category/${category.id}`} key={category.id}>{category.name}</Link>
                     })
+                }
+            </div>
+
+            <div className="space-y-10">
+                {
+                    news.map(ns=><LeftSideNewsCard key={ns._id} news={ns}></LeftSideNewsCard>)
                 }
             </div>
         </div>
---
+++ src/Pages/Shared/LeftSideNav/LeftSideNewsCard.jsx
@@ -0,0 +1,34 @@
+
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import { FaCalendarDays } from 'react-icons/fa6';
+
+const LeftSideNewsCard = ({ news }) => {
+    const { image_url, title } = news;
+
+    console.log(news);
+    return (
+        <div className="card  w-full rounded">
+            <figure>
+                <img
+                    src={image_url}
+                    alt="News Image" />
+            </figure>
+            <div className="space-y-4">
+                <h2 className="text-xl font-semibold">{title}</h2>
+                <div className='flex space-x-4 '>
+                    <p className='font-semibold'>Sports </p>
+                    <p className='text-gray-500'>
+                        <span className='flex gap-3 items-center'><FaCalendarDays></FaCalendarDays>{moment().format("MMM D, YYYY")}</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+LeftSideNewsCard.propTypes = {
+    news: PropTypes.object.isRequired
+};
+
+export default LeftSideNewsCard;
