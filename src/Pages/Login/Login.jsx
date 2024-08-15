8cd45a0eb8c060c4c2a74f53a1657b31726ce6e2
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-08-15 17:47:14 +0600

    register page added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -16,13 +16,13 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="Enter your email address" className="input input-bordered" required />
+                                <input type="email" name="email" placeholder="Enter your email address" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="Enter your password" className="input input-bordered" required />
+                                <input type="password" name="password" placeholder="Enter your password" className="input input-bordered" required />
                                 <label className="label">
                                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                 </label>
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,53 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Register = () => {
     return (
-        <div>
-            register
+        <div className="bg-[#F3F3F3]">
+            <Navbar></Navbar>
+            <div className="  min-h-screen pt-10">
+                <div className="hero-content flex-col ">
+                    <div className="card bg-base-100 lg:w-1/2 shrink-0 shadow-2xl p-3 md:p-10">
+                        <h1 className="text-3xl font-bold text-center mb-10">Register your account</h1>
+                        <hr />
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Your Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="Enter your name" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="Enter your email address" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo Url</span>
+                                </label>
+                                <input type="text" name="photoUrl" placeholder="Enter your photo url" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="Enter your password" className="input input-bordered" required />
+                                <label className="label flex gap-2">
+                                    <input type="radio" name="radio-1" className="radio" /> 
+                                    <p><span> Accept</span><span className="font-semibold">Term & Condition</span></p>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn bg-black text-white">Register</button>
+                                <p className="text-center my-6 font-semibold">Already Have An Account? <span className="text-red-600"><Link to={'/login'}>Login</Link></span></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
