bad17f5baffa7eb56cbe0c86952a508e9cf00fae
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-08-16 22:20:07 +0600

    project completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -15,13 +15,13 @@ const Home = () => {
 
             <Navbar></Navbar>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-                <div className="border-2">
+                <div className="">
                     <LeftSideNav></LeftSideNav>
                 </div>
-                <div className="md:col-span-2 border-2">
+                <div className="md:col-span-2 ">
                     <MainNews></MainNews>
                 </div>
-                <div className="border-2">
+                <div className="">
                     <RightSideNav></RightSideNav>
                 </div>
             </div>
